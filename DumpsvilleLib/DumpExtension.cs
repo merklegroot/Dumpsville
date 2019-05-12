5f4859fef3cacb46ae2e464487800a785f2cab71
Merkle Groot <merklegroot@gmail.com>
2019-05-11 23:05:24 -0400

    initial commit

---
+++ DumpsvilleLib/DumpExtension.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DumpsvilleLib
+{
+    public static class DumpExtension
+    {
+        public static void Dump(this object item)
+        {
+            if (item == null) { Console.WriteLine("[null]"); return; }
+            if (item is string itemString)
+            {
+                if (itemString == string.Empty)
+                {
+                    Console.WriteLine("[empty string]");
+                    return;
+                }
+
+                Console.WriteLine(itemString);
+                return;
+            }
+
+            var contents = JsonConvert.SerializeObject(item, Formatting.Indented);
+            Console.WriteLine(contents);
+        }
+    }
+}
